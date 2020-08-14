8cd81d7b92060aedb70ddd33ae38ac2761f6491c
Nikhil Pathak <nkp2dec1999@gmail.com>
2020-08-14 21:57:25 +0530

    Very Easy Question commit

---
+++ Assignment 1/Very Easy Question/Q1.js
@@ -0,0 +1,7 @@
+function convert(min){
+    return min*60;
+}
+
+console.log(convert(5));
+console.log(convert(3));
+console.log(convert(2));
---
+++ Assignment 1/Very Easy Question/Q2.js
@@ -0,0 +1,10 @@
+function dividesEvenly(a,b){
+    if(a%b == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(dividesEvenly(98,7));
+console.log(dividesEvenly(85,4));
